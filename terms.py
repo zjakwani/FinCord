af4cce553c325c837a94a4e9f4f3d3b733b21f9c
lievan <0evanli0@gmail.com>
2022-01-05 18:58:46 -0500

    scraped financial terms

---
+++ terms.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+import csv
+
+dr =  webdriver.Chrome(executable_path='/Users/evanli/NLP/song-project/chromedriver')
+
+dr.get('https://www.nysscpa.org/professional-resources/accounting-terminology-guide#sthash.rzLLuZGo.dpbs')
+
+sections = dr.find_elements_by_css_selector('html > body > form > main > div > div > div > div')
+
+with open('financial_terms.csv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    for section in sections:
+        names = section.find_elements_by_css_selector('h3')
+        if names and not names[0].text:
+            names = names[1:] #blank h3 header in some random sections
+        descriptions = section.find_elements_by_css_selector('p')
+        for name, description in zip(names, descriptions):
+            name = name.text.replace('\n', '').replace('\t', '').replace('\"', '').strip()
+            description = description.text.replace('\n', '').replace('\t', '').replace('\"', '').strip()
+            writer.writerow([name, description])
+    
