a85563eeb31f7280ad42a05ba77d2f739659a2b3
prabhar2042 <33375822+prabhar2042@users.noreply.github.com>
2022-01-05 23:14:45 -0500

    multiple words

--- basicbot.py
+++ basicbot.py
@@ -29,14 +29,39 @@ async def on_message(message):
         mydict[k] = v
     text = '{0.content}'.format(message).split()
     print(text)
-    for word in text:
+    twoFound = False
+    span = 2
+    doubleOne = [" ".join(text[i:i+span]) for i in range(0, len(text), span)]
+    doubleTwo = [" ".join(text[i:i+span]) for i in range(1, len(text), span)]
+
+    for word in doubleOne:
         lemma = lemmatizer.lemmatize(word.lower())
         if lemma in mydict:
+            print("doubleOne")
             reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
             await message.reply(reply, mention_author=True)
             print("recognized")
-        
+            twoFound = True
+    
+    if not twoFound:
+        for word in doubleTwo:
+            lemma = lemmatizer.lemmatize(word.lower())
+            if lemma in mydict:
+                print("doubleTwo")
+                reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
+                await message.reply(reply, mention_author=True)
+                print("recognized")
+                twoFound = True
 
+    if not twoFound:
+        for word in text:
+            lemma = lemmatizer.lemmatize(word.lower())
+            if lemma in mydict:
+                print("orig")
+                reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
+                await message.reply(reply, mention_author=True)
+                print("recognized")
+                twoFound = True
 
 key = open('key.txt').read()
 bot.run(key)
