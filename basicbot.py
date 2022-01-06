1cc36665023adf002bebd5d87a82d9b047cd5f60
prabhar2042 <33375822+prabhar2042@users.noreply.github.com>
2022-01-05 20:11:32 -0500

    sample dictionary cmd line

--- basicbot.py
+++ basicbot.py
@@ -1,11 +1,25 @@
 import discord
+import csv
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        
 
     async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
+        mydict = {}
+        reader = csv.reader(open("data.csv", "r"))
+        for rows in reader:
+            k = rows[0]
+            v = rows[1]
+            mydict[k] = v
+        text = '{0.content}'.format(message).split()
+        print(text)
+        for word in text:
+            if word in mydict:
+                print('FinBot recognized ' + word  +': ' + mydict[word])
+        
 
 client = MyClient()
-client.run('OTI4NDExNzU2MjE1Mjc5NzAw.YdYY0w.-jwMwEW2rGNrUPH9Mn28aMLbAAE')
+client.run('OTI4NDExNzU2MjE1Mjc5NzAw.YdYY0w.8CluFQ5XKcihfRTZOM7pdl63JrQ')
