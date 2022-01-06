5355de042c5e3561b94a68103fdc5746f8bd918c
Matthew <34844561+Matthew10241729@users.noreply.github.com>
2022-01-05 23:07:00 -0500

    nlp

--- basicbot.py
+++ basicbot.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import pickle
 
 import discord
 import nltk
@@ -7,10 +8,25 @@ import nltk
 nltk.download('wordnet')
 from discord.ext import commands
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 lemmatizer = WordNetLemmatizer()
-
 bot = commands.Bot(command_prefix='!')
+
+mydict = {}
+reader = csv.reader(open("financial_terms.csv", "r"))
+for rows in reader:
+    k = lemmatizer.lemmatize(rows[0].lower())
+    v = rows[1]
+    mydict[k] = v
+
+f = open('nlp/classifier.pickle', 'rb')
+classifier = pickle.load(f)
+f.close()
+
+f = open('nlp/vectorizer.pickle', 'rb')
+vectorizer = pickle.load(f)
+f.close()
 
 @bot.event
 async def on_ready():
@@ -20,21 +36,18 @@ async def on_ready():
 async def on_message(message):
     if message.author == bot.user:
         return
-
-    mydict = {}
-    reader = csv.reader(open("financial_terms.csv", "r"))
-    for rows in reader:
-        k = lemmatizer.lemmatize(rows[0].lower())
-        v = rows[1]
-        mydict[k] = v
     text = '{0.content}'.format(message).split()
-    print(text)
-    for word in text:
-        lemma = lemmatizer.lemmatize(word.lower())
-        if lemma in mydict:
-            reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
-            await message.reply(reply, mention_author=True)
-            print("recognized")
+    
+    stext = ' '.join(text)
+    ttext = vectorizer.transform([stext]) # transformed text
+    dact = classifier.predict(ttext)[0] # get dialog act of stext
+    if dact == 'whQuestion':
+        for word in text:
+            lemma = lemmatizer.lemmatize(word.lower())
+            if lemma in mydict:
+                reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
+                await message.reply(reply, mention_author=True)
+                print("recognized")
         
 
 
