7a8b8b40435366f314c4ccc3150b5d3a8aef98f3
Zain Jakwani <zjakwani@gmail.com>
2022-01-06 13:05:03 -0700

    changes

--- basicbot.py
+++ basicbot.py
@@ -76,8 +76,9 @@ async def on_message(message):
 
 card_info = creditinfo.get_card_info('credit_info.csv')
 
-@bot.command(name='compare', help='compare capital one credit cards')
-async def compare(ctx, c1: str, c2: str):
+@bot.command(name='compare', help='Compare capital one credit cards. Input card names seperated by /')
+async def compare(ctx, *, message):
+    c1, c2 = message.split('/')
     res = creditinfo.compare_cards(c1, c2, card_info)
     await ctx.send(res)
         
