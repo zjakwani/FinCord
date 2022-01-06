a7ec9bc9448521f5c1e8a88ae1fef0c94259307b
prabhar2042 <33375822+prabhar2042@users.noreply.github.com>
2022-01-05 21:41:48 -0500

    lemmatization

--- basicbot.py
+++ basicbot.py
@@ -2,6 +2,12 @@ import discord
 import csv
 import os
 
+import nltk
+nltk.download('wordnet')
+from nltk.stem import WordNetLemmatizer
+ 
+lemmatizer = WordNetLemmatizer()
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -9,19 +15,27 @@ class MyClient(discord.Client):
         
 
     async def on_message(self, message):
+        if message.author.id == self.user.id:
+            return
+
         mydict = {}
         reader = csv.reader(open("financial_terms.csv", "r"))
         for rows in reader:
-            k = rows[0].lower()
+            k = lemmatizer.lemmatize(rows[0].lower())
             v = rows[1]
             mydict[k] = v
         text = '{0.content}'.format(message).split()
         print(text)
         for word in text:
-            if word.lower() in mydict:
-                print('FinBot recognized ' + word  +': ' + mydict[word])
+            lemma = lemmatizer.lemmatize(word.lower())
+            if lemma in mydict:
+                reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
+                await message.reply(reply, mention_author=True)
+                print("recognized")
         
 
 
+key = open('key.txt').read()
 client = MyClient()
 client.run(key)
+
