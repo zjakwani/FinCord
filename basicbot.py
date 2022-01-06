039756f779c3817eb854de4ff0bd4b03901b99bc
prabhar2042 <33375822+prabhar2042@users.noreply.github.com>
2022-01-05 21:01:18 -0500

    reading from main dict

--- basicbot.py
+++ basicbot.py
@@ -1,5 +1,6 @@
 import discord
 import csv
+import os
 
 
 class MyClient(discord.Client):
@@ -9,19 +10,18 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
         mydict = {}
-        reader = csv.reader(open("data.csv", "r"))
+        reader = csv.reader(open("financial_terms.csv", "r"))
         for rows in reader:
-            k = rows[0]
+            k = rows[0].lower()
             v = rows[1]
             mydict[k] = v
         text = '{0.content}'.format(message).split()
         print(text)
         for word in text:
-            if word in mydict:
+            if word.lower() in mydict:
                 print('FinBot recognized ' + word  +': ' + mydict[word])
         
 
 
-key = open('key.txt').read()
 client = MyClient()
 client.run('OTI4NDExNzU2MjE1Mjc5NzAw.YdYY0w.8CluFQ5XKcihfRTZOM7pdl63JrQ')
