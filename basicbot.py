7513786b4b34e7672457ade9d99d27a0f1cca277
Zain Jakwani <zjakwani@gmail.com>
2022-01-05 19:52:19 -0700

    refactored class based to script

--- basicbot.py
+++ basicbot.py
@@ -1,41 +1,43 @@
-import discord
 import csv
 import os
 
+import discord
 import nltk
+
 nltk.download('wordnet')
+from discord.ext import commands
 from nltk.stem import WordNetLemmatizer
- 
+
 lemmatizer = WordNetLemmatizer()
 
+bot = commands.Bot(command_prefix='!')
 
-class MyClient(discord.Client):
-    async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+@bot.event
+async def on_ready():
+    print('Logged on as {0.user}!'.format(bot))
         
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
 
-    async def on_message(self, message):
-        if message.author.id == self.user.id:
-            return
-
-        mydict = {}
-        reader = csv.reader(open("financial_terms.csv", "r"))
-        for rows in reader:
-            k = lemmatizer.lemmatize(rows[0].lower())
-            v = rows[1]
-            mydict[k] = v
-        text = '{0.content}'.format(message).split()
-        print(text)
-        for word in text:
-            lemma = lemmatizer.lemmatize(word.lower())
-            if lemma in mydict:
-                reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
-                await message.reply(reply, mention_author=True)
-                print("recognized")
+    mydict = {}
+    reader = csv.reader(open("financial_terms.csv", "r"))
+    for rows in reader:
+        k = lemmatizer.lemmatize(rows[0].lower())
+        v = rows[1]
+        mydict[k] = v
+    text = '{0.content}'.format(message).split()
+    print(text)
+    for word in text:
+        lemma = lemmatizer.lemmatize(word.lower())
+        if lemma in mydict:
+            reply = 'FinBot recognized ' + word  +': ' + mydict[lemma]
+            await message.reply(reply, mention_author=True)
+            print("recognized")
         
 
 
 key = open('key.txt').read()
-client = MyClient()
-client.run(key)
+bot.run(key)
 
